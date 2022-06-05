5a6dfcc5fa2a56dc2ae81e4c0c51e90baa60e783
Marcin Lech <arcinech@gmail.com>
2022-06-05 21:36:43 +0200

    Add: Routes and Route to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-
+import {Routes, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Header from './components/views/Header/Header';
+import NotFound from './components/pages/NotFound/NotFound';
+import Home from './components/pages/Home/Home';
+import SingleTable from './components/pages/SingleTable/SingleTable'
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  return (
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tables/:id" element={<SingleTable />}/>
+        <Route path='/tables/*' element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Container>
   );
 }
 
