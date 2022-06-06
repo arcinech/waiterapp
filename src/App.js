be0ba5364353c1dae7d0b593f5082010bf3b9e66
Marcin Lech <arcinech@gmail.com>
2022-06-06 23:06:23 +0200

    Remove: wrong route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Header from './components/views/Header/Header';
 import NotFound from './components/pages/NotFound/NotFound';
 import Home from './components/pages/Home/Home';
-import SingleTable from './components/pages/SingleTable/SingleTable'
+import SingleTable from './components/pages/SingleTable/SingleTable';
+import Footer from './components/views/Footer/Footer';
+
 function App() {
 
   return (
@@ -12,9 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tables/:id" element={<SingleTable />}/>
-        <Route path='/tables/*' element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Container>
   );
 }
