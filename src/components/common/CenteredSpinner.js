004f958aac8bea3b35df2135768fd4413fce613c
Marcin Lech <arcinech@gmail.com>
2022-06-06 23:06:41 +0200

    Add: centered spinner component

---
+++ src/components/common/CenteredSpinner.js
@@ -0,0 +1,13 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+const CenteredSpinner = () => {
+  <div className="d-flex my-2">
+        <Spinner 
+          animation="border" 
+          variant="primary"
+          className="mx-auto"
+          />
+      </div>
+}
+
+export default CenteredSpinner;
